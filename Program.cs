3a94ced73710debb1368481e35ea82236da9017b
James Watson <jameywatson134@gmail.com>
2023-08-02 08:28:07 -0700

    initial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//1. Iterate and print values
+
+static void PrintList(List<string> MyList)
+{
+
+    foreach(string names in MyList){
+        Console.WriteLine(names);
+    }
+}
+List<string> TestStringList = new List<string>() {"Harry", "Steve", "Carla", "Jeanne"};
+PrintList(TestStringList);
+
+
